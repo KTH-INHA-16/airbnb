21aaf02425b0b4e2fa0cf2fe4eb9729f807944e2
KTH-INHA-16 <kim1120318@naver.com>
2020-07-20 17:59:47 +0900

    third version
    third

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
+
+"""decorator"""
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    """list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )"""
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avator",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )

d487ae432dcadd18ffe530a933f756a8968c2929
KTH-INHA-16 <kim1120318@naver.com>
2020-08-06 18:02:00 +0900

    열번째
    로그인 구현 form 과 class view 방식으로
    
    clean_으로 구현

--- users/views.py
+++ users/views.py
@@ -1,4 +1,19 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
-# render
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        return render(request, "users/login.html", {"form": form})
+
+
+"""def login_view(request):
+    if request.method =="GET":
+        pass
+    elif request.method =="P":"""
